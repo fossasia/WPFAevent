b8e3c8b73751683a38b55e79365a000438dd377d
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2026-01-18 02:20:35 +0600

    feat(templates): hide PHP templates when block theme active

--- includes/class-wpfaevent-templates.php
+++ includes/class-wpfaevent-templates.php
@@ -26,6 +26,11 @@ class WPFA_Templates {
 	}
 
 	public static function register( $templates ) {
+		// Don't register templates for block themes - they use block-based templates only
+		if ( function_exists( 'wp_is_block_theme' ) && wp_is_block_theme() ) {
+			return $templates;
+		}
+
 		foreach ( self::$templates as $file => $label ) {
 			$templates[ $file ] = __( $label, 'wpfaevent' );
 		}
@@ -33,6 +38,11 @@ class WPFA_Templates {
 	}
 
 	public static function load( $template ) {
+		// Don't load templates for block themes
+		if ( function_exists( 'wp_is_block_theme' ) && wp_is_block_theme() ) {
+			return $template;
+		}
+
 		if ( is_singular( 'page' ) ) {
 			$chosen = get_page_template_slug( get_queried_object_id() );
 			if ( isset( self::$templates[ $chosen ] ) ) {
