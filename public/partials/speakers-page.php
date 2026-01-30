47351de4468e5542a70b43c42600a6992442b191
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2026-01-30 22:18:53 +0600

    Remove redundant files

--- public/partials/speakers-page.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) { exit; }
-// Proxy to existing speakers page template
-$template = plugin_dir_path( __FILE__ ) . 'public/partials/speakers-page-content.php';
-if ( file_exists( $template ) ) {
-    include_once $template;
-} else {
-    echo '<!-- speakers page template missing -->';
-}
