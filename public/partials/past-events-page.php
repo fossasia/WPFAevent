b33e14c145ad313ad84fd7767a4fd9035daabc95
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2026-01-30 23:48:48 +0600

    remove redundant file

--- public/partials/past-events-page.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) { exit; }
-// Proxy to existing past events template
-$template = dirname( dirname( __FILE__ ) ) . '/../../templates/past-events-page.php';
-if ( file_exists( $template ) ) {
-    include_once $template;
-} else {
-    echo '<!-- past events template missing -->';
-}
