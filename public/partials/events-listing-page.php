63170747df1cf75c3335b3c81e2a0d1b68ac7718
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2026-02-10 14:11:05 +0600

    remove redundant files

--- public/partials/events-listing-page.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) { exit; }
-// Proxy to existing events listing template
-$template = dirname( dirname( __FILE__ ) ) . '/../../templates/events-listing-page.php';
-if ( file_exists( $template ) ) {
-    include_once $template;
-} else {
-    echo '<!-- events listing template missing -->';
-}
