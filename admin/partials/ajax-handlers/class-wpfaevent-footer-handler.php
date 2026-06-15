d7f74c7ef047d6f1a90c222f925c735c0e689184
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2026-06-15 08:52:04 +0600

    Refactor AJAX handlers to resolve PHPStan unused property and parameter errors

--- admin/partials/ajax-handlers/class-wpfaevent-footer-handler.php
+++ admin/partials/ajax-handlers/class-wpfaevent-footer-handler.php
@@ -19,33 +19,11 @@
 class Wpfaevent_Footer_Handler {
 
 	/**
-	 * The plugin name.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $plugin_name    The plugin name.
-	 */
-	private $plugin_name;
-
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
-	private $version;
-
-	/**
 	 * Initialize the class.
 	 *
 	 * @since    1.0.0
-	 * @param    string $plugin_name    The name of this plugin.
-	 * @param    string $version        The version of this plugin.
 	 */
-	public function __construct( $plugin_name, $version ) {
-		$this->plugin_name = $plugin_name;
-		$this->version     = $version;
+	public function __construct() {
 	}
 
 	/**
