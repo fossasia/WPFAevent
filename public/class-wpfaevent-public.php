4b6d5557f18a8a4fc269ee8dac70977687c0cf07
Nimeshan <nimeshan995@gmail.com>
2026-06-25 20:58:14 +0530

    style: split additional information stylesheet

--- public/class-wpfaevent-public.php
+++ public/class-wpfaevent-public.php
@@ -301,6 +301,14 @@ class Wpfaevent_Public {
 			'all'
 		);
 
+		wp_register_style(
+			$this->plugin_name . '-additional-information',
+			WPFAEVENT_URL . 'public/css/templates/additional-information.css',
+			array( $this->plugin_name, $this->plugin_name . '-navigation' ),
+			$this->version,
+			'all'
+		);
+
 		wp_register_script(
 			$this->plugin_name . '-speakers',
 			plugin_dir_url( __FILE__ ) . 'js/wpfaevent-speakers.js',
@@ -456,6 +464,10 @@ class Wpfaevent_Public {
 
 		if ( is_singular( 'wpfa_event' ) ) {
 			wp_enqueue_style( $this->plugin_name . '-single-event' );
+		}
+
+		if ( $this->is_wpfa_template_file_active( 'page-additional-information.php' ) ) {
+			wp_enqueue_style( $this->plugin_name . '-additional-information' );
 		}
 
 		/**
