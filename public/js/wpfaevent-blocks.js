a2254d2519f4fcfdb4c1a82876f78c9e6cf3caf6
Nimeshan <nimeshan995@gmail.com>
2026-08-13 10:55:32 +0530

    fix: restore js lint fixes without config changes

--- public/js/wpfaevent-blocks.js
+++ public/js/wpfaevent-blocks.js
@@ -1,36 +1,38 @@
-( function( blocks, element, i18n, serverSideRender ) {
+(function (blocks, element, i18n, serverSideRender) {
 	'use strict';
 
-	var el = element.createElement;
-	var __ = i18n.__;
-	var ServerSideRender = serverSideRender;
-	var templates = window.wpfaeventBlocks || [];
+	const el = element.createElement;
+	const __ = i18n.__;
+	const ServerSideRender = serverSideRender;
+	const templates = window.wpfaeventBlocks || [];
 
-	templates.forEach(
-		function( template ) {
-			blocks.registerBlockType(
-				template.name,
-				{
-					apiVersion: 2,
-					title: template.title,
-					icon: 'calendar-alt',
-					category: 'widgets',
-					description: __( 'Display WPFA event content.', 'wpfaevent' ),
-					supports: {
-						align: [ 'wide', 'full' ],
-					},
-					edit: function() {
-						return el(
-							'div',
-							{ className: 'wpfaevent-block-preview' },
-							ServerSideRender ? el( ServerSideRender, { block: template.name } ) : template.title
-						);
-					},
-					save: function() {
-						return null;
-					},
-				}
-			);
-		}
-	);
-} )( window.wp.blocks, window.wp.element, window.wp.i18n, window.wp.serverSideRender );
+	templates.forEach(function (template) {
+		blocks.registerBlockType(template.name, {
+			apiVersion: 2,
+			title: template.title,
+			icon: 'calendar-alt',
+			category: 'widgets',
+			description: __('Display WPFA event content.', 'wpfaevent'),
+			supports: {
+				align: ['wide', 'full'],
+			},
+			edit() {
+				return el(
+					'div',
+					{ className: 'wpfaevent-block-preview' },
+					ServerSideRender
+						? el(ServerSideRender, { block: template.name })
+						: template.title
+				);
+			},
+			save() {
+				return null;
+			},
+		});
+	});
+})(
+	window.wp.blocks,
+	window.wp.element,
+	window.wp.i18n,
+	window.wp.serverSideRender
+);
